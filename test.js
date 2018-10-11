0a1f7c0e9bc26f874adbf040abe7ee812f041a36
pragnesh patel <Praggers@gmail.com>
2018-10-11 09:03:39 +0100

    Update test.js

--- test.js
+++ test.js
@@ -5,17 +5,9 @@
  // npm --save-dev geckodriver
  // npm --save-dev chromedriver
 
- // run test using node test.js
- // src :  http://www.codechewing.com/library/beginners-guide-to-selenium-webdriverjs/
-
-
 var WebDriver = require('selenium-webdriver');
 require('geckodriver'); //for firefox
 require('chromedriver');
-
-// var driver = new WebDriver.Builder().withCapabilities(
-//     WebDriver.Capabilities.chrome()
-// ).build();
 
 var driver = new WebDriver.Builder().forBrowser('chrome').build();
  
@@ -27,14 +19,4 @@ driver.findElement(WebDriver.By.name('q')).then(function(input) {
 });
  
 driver.sleep(1000);
- 
-// var elem = driver.findElement({id: 'ires'});
-// var link = elem.findElement({css: 'ol li h3 a'});
- 
-// link.getText().then(function(text) {
-//   var pageTitle = 'Code Chewing –HTML, PHP, CSS, Javascript tips';
-//   console.log('codechewing.com is the top result?');
-//   console.log(text.indexOf(pageTitle) !== -1);
-// });
-
 driver.quit();
